ec52212c0d170881dee7b8f984a7478072dd9ba5
ibragim-programist <147244004+ibragim-programist@users.noreply.github.com>
2025-07-25 12:37:21 +0300

    Delete utility/getPaths.js

--- utility/getPaths.js
+++
@@ -1,7 +0,0 @@
-const paths = [
-    'D:/Web/step_in_feature/server/data/Users.txt',
-    'D:/Web/step_in_feature/server/data/Rooms.txt',
-    'D:/Web/step_in_feature/server/data/Messages'
-]
-
-export default paths
