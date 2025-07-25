1b4d793b784c4a4cc9e217d44e8f90f074e77215
ibragim-programist <147244004+ibragim-programist@users.noreply.github.com>
2025-07-25 12:37:46 +0300

    Delete utility directory

--- utility/security/__getNotAllowedWords.js
+++
@@ -1,8 +0,0 @@
-export const notAllowedWords = [
-    '<script>', '</script>', 
-    '<div>', '</div>',
-    '<iframe>', '</iframe>',
-    '<img>', '<svg>',
-    'javascript:', 'onload=',
-    'onerror=', 'onclick='
-];
